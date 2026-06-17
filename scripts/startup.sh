b3ca95b98f9ed07f8b7af3224cb0e7942b03c4b3
AveryFrog <pixels128yt@gmail.com>
2026-06-17 10:01:21 +0100

    Adds default theme file if none exists

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,4 +5,8 @@ SCRIPT_DIR=$(dirname "$0")
 mkdir -p ~/.config/qualia-shell
 touch ~/.config/qualia-shell/settings.json
 
+if [ ! -f ~/.config/qualia-shell/theme.json ]; then
+    cp $SCRIPT_DIR/../theme.json ~/.config/qualia-shell/theme.json
+fi
+
 qs -p $SCRIPT_DIR/../shell.qml
